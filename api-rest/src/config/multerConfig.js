998b130cbd7156a4eeea8d4fadf2f1f172ce0775
Wilson Magalhães <wilson.magalhaes@gmail.com>
2022-02-05 01:10:13 -0300

    finalizando fotos

--- api-rest/src/config/multerConfig.js
+++ api-rest/src/config/multerConfig.js
@@ -1,18 +1,28 @@
 import multer from 'multer'
 import {extname, resolve } from 'path';
+import { createBrotliCompress } from 'zlib';
 
 
 const aleatorio = () => Math.floor(Math.random()* 10000 + 10000)
 
 
 export default {
-    storage: multer.diskStorage({
-      destination: (req, file,callback) =>{
-        callback(null,resolve(__dirname,'..','..','uploads'));
-        },
-        filename: (req,file,callback) => {
-            callback(null, `${Date.now()}_${aleatorio()}_${extname(file.originalname)}`);
-        },
-    }),
+
+  fileFilter: (req,file,cb) => {
+    if( file.mimetype !== 'image/png' && file.mimetype !== 'image/jpeg'){
+      return cb(new multer.MulterError("Arquivo precisa ser PNG ou PNG"));
+    }
+
+    return cb(null, true);
+
+  },
+  storage: multer.diskStorage({
+    destination: (req, file,cb) =>{
+      cb(null,resolve(__dirname,'..','..','uploads'));
+      },
+      filename: (req,file,cb) => {
+          cb(null, `${Date.now()}_${aleatorio()}_${extname(file.originalname)}`);
+      },
+  }),
 
 };
--- api-rest/src/controllers/FotoController.js
+++ api-rest/src/controllers/FotoController.js
@@ -1,8 +1,19 @@
+import multer from 'multer';
+import multerConfig from "../config/multerConfig";
+const upload = multer(multerConfig).single('foto');
+
+
 class FotoController {
   async store(req,res){
-    res.json(req.file);
+    return upload(req,res,(error) => {
+      if(error){
+        return res.status(400).json({
+          errors: [error.code],
+        });
+      }
+      res.json(req.file);
+    });
   }
 }
-
 
 export default new FotoController();
